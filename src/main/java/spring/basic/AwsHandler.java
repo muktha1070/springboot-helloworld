7176807df12902ea84019527987ec5ad8f974969
muktha1070 <114932306+muktha1070@users.noreply.github.com>
2022-12-26 21:56:03 +0530

    Create AwsHandler.java

---
+++ src/main/java/spring/basic/AwsHandler.java
@@ -0,0 +1,6 @@
+package spring.basic;
+
+import org.springframework.cloud.function.adapter.aws.SpringBootRequestHandler;
+
+public class AWSHandler extends SpringBootRequestHandler<String,Object> {
+}
