92478610942fd742061f4643e1896a8efe7bdfbb
muktha1070 <114932306+muktha1070@users.noreply.github.com>
2022-12-26 18:36:24 +0530

    Delete AwsHandler.java

--- src/main/java/spring/basic/AwsHandler.java
+++
@@ -1,6 +0,0 @@
-package com.muktha.aws.lambda;
-
-import org.springframework.cloud.function.adapter.aws.SpringBootRequestHandler;
-
-public class AwsHandler extends SpringBootRequestHandler<String,Object> {
-}
